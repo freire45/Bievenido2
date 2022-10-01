f89e7217892bd81d32656ab4ad44996199b8cac8
Erick Freire <erilefr@gmail.com>
2022-10-01 19:15:05 -0300

    a
    a

---
+++ Bievenido2/src/br/com/erickfreire/bievenido2/Bievenido2.java
@@ -0,0 +1,17 @@
+package br.com.erickfreire.bievenido2;
+
+/**
+ * Bievenido a programacion en Java - Version 2
+ * @author Erick Freire
+ * @version 1 - Creado en 01-10-2022
+ *  */
+
+public class Bievenido2 {
+
+	public static void main(String[] args) {
+		System.out.print("Bievenido a ");
+		System.out.println("Programacion en Java!");
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bievenido2
  Bievenido a programacion en Java - Version 2
+
+## Image
+![Currículo no Computador](bievenido_2.png)
+
